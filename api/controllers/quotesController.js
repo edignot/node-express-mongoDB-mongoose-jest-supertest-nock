20b1cfaa3e38156a8f9cf0ff2c1e82ce01183a59
Ed Ignot <edignot@gmail.com>
2020-11-02 16:32:36 -0700

    CRUD operation controllers

--- api/controllers/quotesController.js
+++ api/controllers/quotesController.js
@@ -1,19 +1,51 @@
-exports.get_quotes = async (req, res) => {
-  res.json('get quotes')
+const Quote = require('../models/quoteSchema')
+
+exports.get_quotes = (req, res) => {
+  Quote.find()
+    .then((quotes) =>
+      quotes.length
+        ? res.status(200).json(quotes)
+        : res.status(200).json({ message: 'No quotes found' }),
+    )
+    .catch((error) =>
+      res.status(500).json({ message: 'Something went wrong', error }),
+    )
 }
 
-exports.post_quote = async (req, res) => {
-  res.json('post quote')
+exports.post_quote = (req, res) => {
+  const newQuote = new Quote({
+    quote: req.body.quote,
+    author: req.body.author,
+  })
+
+  newQuote
+    .save()
+    .then(() => res.status(200).json(newQuote))
+    .catch((error) =>
+      res.status(500).json({ message: 'Something went wrong', error }),
+    )
 }
 
 exports.get_quote = async (req, res) => {
-  res.json('get quote')
+  Quote.findById(req.params.id)
+    .then((quote) => res.status(200).json(quote))
+    .catch((error) =>
+      res.status(404).json({ message: 'Quote not found', error }),
+    )
 }
 
 exports.update_quote = async (req, res) => {
-  res.json('update quote')
+  Quote.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  })
+    .then((quote) => res.status(200).json(quote))
+    .catch((error) =>
+      res.status(404).json({ message: 'Quote not found', error }),
+    )
 }
 
 exports.delete_quote = async (req, res) => {
-  res.json('delete quote')
+  Quote.findByIdAndDelete(req.params.id).then(() =>
+    res.json({ message: 'Quote deleted' }),
+  )
 }
--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true })
+
+mongoose.connection.on('error', () => {
+  console.log('Error connecting database')
+})
+
+mongoose.connection.once('open', () => {
+  console.log('Database successfully connected')
+})
+
 const app = require('./app')
 
 const port = process.env.PORT || 5000
