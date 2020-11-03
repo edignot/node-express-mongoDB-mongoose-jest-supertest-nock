c5fb6d167e8e5ef9bfd9d8b7173cf76406c589a8
Ed Ignot <edignot@gmail.com>
2020-11-03 10:39:49 -0700

    Test data file created

---
+++ api/tests/quotes.test.data.js
@@ -0,0 +1,7 @@
+const testQuotes = [
+  { quote: 'quote1', author: 'author1' },
+  { quote: 'quote2', author: 'author2' },
+  { quote: 'quote3', author: 'author3' },
+]
+
+module.exports = testQuotes
